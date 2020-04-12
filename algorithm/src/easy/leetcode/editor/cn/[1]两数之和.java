5beef2cd50bb5256376ca9891f2a78d67f32a244
hanfeng <hanfeng21050@163.com>
2020-04-12 11:18:12 +0800

    readme

---
+++ algorithm/src/easy/leetcode/editor/cn/[1]两数之和.java
@@ -0,0 +1,36 @@
+package easy.leetcode.editor.cn;
+import easy.leetcode.editor.node.*;
+class question_1{
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。 
+//
+// 示例: 
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+// 
+// Related Topics 数组 哈希表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class  Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] num = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                {
+                    num[0] = i;
+                    num[1] = j;
+                    return num;
+                }
+            }
+        }
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+}
