3cb3d43d82d7fd7481bcb269e486007a6a740fbd
hanfeng <hanfeng21050@163.com>
2020-04-12 12:34:15 +0800

    回文数[easy]

--- algorithm/src/easy/leetcode/editor/cn/[1]两数之和.java
+++ algorithm/src/easy/leetcode/editor/cn/[1]两数之和.java
@@ -1,5 +1,9 @@
 package easy.leetcode.editor.cn;
 import easy.leetcode.editor.node.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 class question_1{
 //给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
 //
@@ -18,6 +22,24 @@ class question_1{
 //leetcode submit region begin(Prohibit modification and deletion)
 class  Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x) && map.get(x) != i) {
+                return new int[]{i,map.get(x)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+/*
+ 解题思路
+   1. 暴力法   时间复杂度:O(n^2)  空间复杂度:O(1)
+    public int[] twoSum(int[] nums, int target) {
         int[] num = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -31,6 +53,36 @@ class  Solution {
         }
         return null;
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+   2. 两遍哈希表法  时间复杂度:O(n)  空间复杂度:O(n)
+   思路是先将元素存进哈希表,然后查找
+   public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x) && map.get(x) != i) {
+                return new int[]{i,map.get(x)};
+            }
+        }
+
+        return null;
+    }
+
+   3. 一遍哈希表法  时间复杂度:O(n)  空间复杂度:O(n)
+   在两边哈希表的基础上进行优化,当我们在插入哈希表的时候先查询是否有满足条件的值,如果有,就结束程序了
+   Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x) && map.get(x) != i) {
+                return new int[]{i,map.get(x)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+
+ */
+
 }
