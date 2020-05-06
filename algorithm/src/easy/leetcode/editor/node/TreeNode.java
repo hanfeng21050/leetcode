d590969764c6b02d53e65158d5786953a9178714
hanfeng <hanfeng21050@163.com>
2020-05-06 12:07:52 +0800

    [83]删除排序链表中的重复元素 [88]合并两个有序数组 [100]相同的树

---
+++ algorithm/src/easy/leetcode/editor/node/TreeNode.java
@@ -0,0 +1,15 @@
+package easy.leetcode.editor.node;
+
+/**
+ * @Author CoolWind
+ * @Date 2020/5/6 11:33
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
